e7d6407204fb0b94f64949db43727cc5ae50249c
jaguarshadow <starbucknakari@gmail.com>
2022-12-04 13:34:03 -0600

    day 4
    cleaner overlap logic for "partial" overlaps

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
 
 namespace Day04
 {
@@ -11,20 +12,30 @@ namespace Day04
             Console.WriteLine($"Fully overlapped assignments: {output.full} \nPartially overlapped assignments: {output.partial}");
         }
 
-    static (int full, int partial) FindSharedAssignments(IEnumerable<string> input) {
-        var full_overlap = 0;
-        var partial_overlap = 0;
+        static (int full, int partial) FindSharedAssignments(IEnumerable<string> input) {
+            var full_overlap = 0;
+            var partial_overlap = 0;
 
-        foreach (var line in input) {
-            var sections = line.Split(new char[] { '-', ',' });
-            var a1 = (start: int.Parse(sections[0]), end: int.Parse(sections[1]));
-            var a2 = (start: int.Parse(sections[2]), end: int.Parse(sections[3]));
-            if (a1.start >= a2.start && a2.end >= a1.end
-                || a2.start >= a1.start && a1.end >= a2.end) full_overlap++;
-            if (a1.start <= a2.end && a2.start <= a1.start 
-                || a2.start <= a1.end && a1.start <= a2.start) partial_overlap++;
+            foreach (var line in input) {
+                var sections = line.Split(new char[] { '-', ',' }).Select(x => int.Parse(x)).ToList();
+                var a1 = (start: sections[0], end: sections[1]);
+                var a2 = (start: sections[2], end: sections[3]);
+
+                var nooverlap = a1.end < a2.start || a2.end < a1.start;
+                if (nooverlap)
+                {
+                    Console.WriteLine($"{a1.start}-{a1.end}  ,  {a2.start}-{a2.end}, NO OVERLAP");
+                    continue;
+                }
+                Console.WriteLine($"{a1.start}-{a1.end}  ,  {a2.start}-{a2.end}, XXX");
+                partial_overlap++;
+
+                /*                if (a1.end < a2.start || a2.end < a1.start) continue; 
+                                partial_overlap++;
+                                if (a1.start >= a2.start && a2.end >= a1.end
+                                    || a2.start >= a1.start && a1.end >= a2.end) full_overlap++;*/
+            }
+            return (full_overlap, partial_overlap);
         }
-        return (full_overlap, partial_overlap);
-    }
     }
 }
