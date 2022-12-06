6de49691ac318827fad84ed89d3b8f6251dbff73
jaguarshadow <starbucknakari@gmail.com>
2022-12-06 00:38:52 -0600

    Day 6 - Tuning Trouble

---
+++ Day06/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Day06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText(@"input.txt").ToString();
+
+            var packet_length = 4;
+            var message_length = 14;
+
+            Console.WriteLine($"Part 1: {FindDistinctMarker(input, packet_length)}");
+            Console.WriteLine($"Part 2: {FindDistinctMarker(input, message_length)}");
+        }
+
+        static int FindDistinctMarker(string s, int length) {
+            for (int i = 0; i < s.Length - length; i++) {
+                if (s.Substring(i, length).Distinct().Count() == length) return i + length;
+            }
+            return 0;
+        }
+    }
+}
