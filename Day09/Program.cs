bb0951b4de82249044417f5824d554108b34dac0
jaguarshadow <starbucknakari@gmail.com>
2022-12-09 23:33:45 -0600

    extra credit  day 9 - slithering snake

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -2,19 +2,28 @@
 {
     internal class Program
     {
+        static (int x, int y)[] oldSnake = new (int, int)[10];
         static void Main(string[] args) {
             var input = File.ReadAllLines(@"input.txt");
+
+            Console.WindowHeight = Console.LargestWindowHeight;
+            Console.WindowWidth = Console.LargestWindowWidth;
+            Console.CursorVisible = false;
+            Console.CursorSize = 1;
+            Console.SetCursorPosition(Console.LargestWindowWidth / 2, Console.LargestWindowWidth / 2);
             
-            var part1 = GetTailMovements(input, 2);
+            //var part1 = GetTailMovements(input, 2);
             var part2 = GetTailMovements(input, 10);
             // 6181, 2386
-            Console.WriteLine($"(Part 1) Unique tail points visited: {part1.Distinct().Count()}");
-            Console.WriteLine($"(Part 2) Unique tail points visited: {part2.Distinct().Count()}");
+            //Console.WriteLine($"(Part 1) Unique tail points visited: {part1.Distinct().Count()}");
+            Console.WriteLine($"(Part 2) Unique tail points visited: {part2.Result.Distinct().Count()}");
         }
 
-        static List<(int x, int y)> GetTailMovements(IEnumerable<string> input, int knotCount) {
+        static async Task<List<(int x, int y)>> GetTailMovements(IEnumerable<string> input, int knotCount) {
             var knots = new (int x, int y)[knotCount];
-            var tailVisited = new List<(int, int)>();
+            Array.Fill(knots, (Console.LargestWindowWidth / 2, Console.LargestWindowWidth / 2));
+            oldSnake = knots.ToArray();
+            var tailVisited = new List<(int x, int y)>();
             tailVisited.Add(knots[0]);
             foreach (var motion in input) {
                 var velocity = GetVelocity(motion[0]);
@@ -23,11 +32,12 @@
                 var maxdiff = 1;
                 while (steps > 0) {
                     steps--;
+                    await DrawSnake(knots);
                     var current = 0;
+                    knots[current].x += velocity.x;
+                    knots[current].y += velocity.y;
+                    for (int i = 1; i < knots.Count(); i++) {
 
-                    for(int i = 1; i < knots.Count(); i++) {
-                        knots[current].x += velocity.x;
-                        knots[current].y += velocity.y;
                         // Check distance between knots
                         var diffx = knots[current].x - knots[i].x;
                         var diffy = knots[current].y - knots[i].y;
@@ -44,6 +54,7 @@
                         if (i == knots.Length - 1) tailVisited.Add(knots[i]);
                     }
                 }
+                
             }
             return tailVisited;
         }
@@ -56,6 +67,23 @@
                 case 'D': return (0, -1);
                 default:  return (0,  0);
             }
+        }
+
+        static async Task DrawSnake((int x, int y)[] snake)
+        {
+            Thread.Sleep(10);
+            for (int i = 0; i < snake.Length; i++)
+            {
+                
+                Console.SetCursorPosition(oldSnake[i].x, oldSnake[i].y);
+                Console.Write(' ');
+                
+                Console.SetCursorPosition(snake[i].x, snake[i].y);
+                Console.Write('0');
+            }
+            oldSnake = snake.ToArray();
+            return;
+
         }
     }
 }
